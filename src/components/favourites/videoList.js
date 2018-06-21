0713948f81aec99be9b70a1580ae0cb8bac052cf
kopcinskia <kopcinskia@gmail.com>
2018-06-21 15:44:50 +0200

    Use defaultList in favourites #1

--- src/components/favourites/videoList.js
+++ src/components/favourites/videoList.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import VideoListItem from './videoListItem'
-
 import DefaultList from '../componentList/defaultList'
 
 const VideoList = ({ videoListItems, toggleListItem, getCurrentItem}) => (
